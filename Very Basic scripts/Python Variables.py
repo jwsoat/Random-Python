46685575ebce348502a8a8f8a0b49139f61ac1b5
Jwsoat <jwsoat@users.noreply.github.com>
2019-08-13 15:32:35 +1200

    Version 0.0.5

--- Very Basic scripts/Python Variables.py
+++ Very Basic scripts/Python Variables.py
@@ -2,3 +2,32 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+x = 4
+x = "Sally"
+print(x)
+
+x = "John"
+print(x)
+#double quotes are the same as single quotes:
+x = 'John'
+print(x)
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+x = y = z = "Orange"
+
+print(x)
+print(y)
+print(z)
+x = "awesome"
+print("Python is " + x)
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+x = 5
+y = 10
+print(x + y)
